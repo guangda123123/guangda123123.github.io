ce36032d169921eb7a8c3b5acedc10de482a42dc
guangda123123 <1378130421@qq.com>
2016-11-14 16:36:36 +0800

    11

--- js/123.js
+++ js/123.js
@@ -1,31 +1,42 @@
-var svg =d3.select('svg');
-var r=svg.append('rect');
-var p1 = svg.append('polygon');
-var p2= svg.append('polygon');
-var p3 = svg.append('polygon');
-var p4= svg.append('polygon');
-var p5 = svg.append('polygon');
-var p6= svg.append('polygon');
-var p7 = svg.append('polygon');
-var p8= svg.append('polygon');
 
-r.attr('x',50).attr('y',38).attr('rx',2).attr('ry',2).attr('width',300).attr('height',220).attr('fill','white').attr('stroke-width',5).attr('stroke','black').attr('opacity',0.5);
-p1.attr('points','100,100 100,200 190,150').attr('fill','steelblue').attr('stroke','white').attr('stroke-width',1);
-p2.attr('points','100,100 100,115 120,100').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
-p3.attr('points','100,200 100,185 120,200').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
-p4.attr('points','112,120 112,180 150,150').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
-p5.attr('points','155,150 300,50 300,250').attr('fill','steelblue').attr('stroke','steelblue').attr('stroke-width',1);
-p6.attr('points','302,48 302,100 268,70').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
-p7.attr('points','302,252 302,200 268,230').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
-p8.attr('points','200,150 265,100 265,200').attr('fill','white').attr('stroke','white').attr('stroke-width',1);
+function move() {
+        // var svgctrl = d3.select('svg');
 
+        var cx = $('#cx1').val();
+        var cy = $('#cy1').val();
+        var svg = d3.select('svg');
+        var r = svg.append('rect');
+        var p1 = svg.append('polygon');
+        var p2 = svg.append('polygon');
+        var p3 = svg.append('polygon');
+        var p4 = svg.append('polygon');
+        var p5 = svg.append('polygon');
+        var p6 = svg.append('polygon');
+        var p7 = svg.append('polygon');
+        var p8 = svg.append('polygon');
+        var x1 = cx*1+50;
+        var x2 = cx*1+140;
+        var y1 = cy*1+62;
+        var y2 = cy*1+162;
+        var y3 = cy*1+112;
+        r.attr('x', cx).attr('y', cy).attr('rx', 2).attr('ry',2).attr('width',300).attr('height',220).attr('fill','white').attr('stroke-width',5).attr('stroke','black').attr('opacity',0.5);
+        p1.attr('points', x1 + ',' + y1 + ' ' + x1 + ',' + y2 + ' ' +x2 + ',' + y3).attr('fill', 'steelblue').attr('stroke', 'white').attr('stroke-width', 1);
+        p2.attr('points', (cx*1 + 50) + ',' + (cy*1 + 62) + ' ' + (cx*1 + 50) + ',' + (cy*1 + 77) + ' ' + (cx*1 + 70) + ',' + (cy*1 + 62) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
+        p3.attr('points', (cx*1 + 50 )+ ',' + (cy*1 + 162) + ' ' + (cx*1 + 50) + ',' + (cy*1 + 147 )+ ' ' + (cx*1 + 70) + ',' + (cy*1 + 162) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
+        p4.attr('points', (cx*1 + 62) + ',' + (cy*1 + 82) + ' ' + (cx*1 + 62) + ',' + (cy*1 + 142) + ' ' + (cx*1 + 100) + ',' + (cy*1 + 112) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
+        p5.attr('points', (cx*1 + 105) + ',' + (cy*1 + 112) + ' ' + (cx*1 + 250) + ',' + (cy*1 + 12) + ' ' +( cx*1 + 250) + ',' + (cy*1 + 212) ).attr('fill', 'steelblue').attr('stroke', 'steelblue').attr('stroke-width', 1);
+        p6.attr('points', (cx*1 + 252) + ',' + (cy*1 + 10 )+ ' ' + (cx*1 + 252) + ',' + (cy*1 + 62) + ' ' + (cx*1 + 218) + ',' + (cy*1 + 32) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
+        p7.attr('points', (cx*1 + 252) + ',' + (cy*1 + 214) + ' ' + (cx*1 + 252 )+ ',' + (cy*1 + 162) + ' ' + (cx*1 + 218) + ',' + (cy*1 + 192) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
+        p8.attr('points', (cx*1 + 150) + ',' + (cy*1 + 112 )+ ' ' + (cx*1 + 215) + ',' + (cy*1 + 62 )+ ' ' + (cx*1 + 215) + ',' + (cy*1 + 162) ).attr('fill', 'white').attr('stroke', 'white').attr('stroke-width', 1);
 
+}
+$('#test').bind('click', move)
 
-p4.on("click",function(){
-          d3.select(this)
-                .attr("fill","wheat");
-        });
-p8.on("click",function(){
-          d3.select(this)
-                .attr("fill","wheat");
-        });
+p4.on("click", function () {
+        d3.select(this)
+                .attr("fill", "wheat");
+});
+p8.on("click", function () {
+        d3.select(this)
+                .attr("fill", "wheat");
+});
